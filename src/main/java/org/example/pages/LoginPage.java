0606d1c63a7f1cbcbabd4e817ef3761b4c3b5e5b
Andrey Merkulov <merkulov.andrej@gmail.com>
2024-09-25 23:00:28 +0200

    Refactored LoginTest

--- src/main/java/org/example/pages/LoginPage.java
+++ src/main/java/org/example/pages/LoginPage.java
@@ -7,9 +7,9 @@ import static org.example.core.DriverHolder.getDriver;
 public class LoginPage {
 
     private static final String PUPPY_URL = "http://localhost:8000/";
-    private static final By ENTER_USERNAME = By.xpath("//input[@placeholder='Username']");
-    private static final By ENTER_PASSWORD = By.xpath("//input[@placeholder='Password']");
-    private static final By LOGIN = By.xpath("//input[@value='Log in']");
+    private static final By USERNAME_INPUT_LOCATOR = By.xpath("//input[@placeholder='Username']");
+    private static final By PASSWORD_INPUT_LOCATOR = By.xpath("//input[@placeholder='Password']");
+    private static final By LOGIN_BUTTON_LOCATOR = By.xpath("//input[@value='Log in']");
 
     public LoginPage openPuppyPage() {
         getDriver().get(PUPPY_URL);
@@ -17,17 +17,17 @@ public class LoginPage {
     }
 
     public LoginPage enterUsername(String input) {
-        getDriver().findElement(ENTER_USERNAME).sendKeys(input);
+        getDriver().findElement(USERNAME_INPUT_LOCATOR).sendKeys(input);
         return this;
     }
 
     public LoginPage enterPassword(String input) {
-        getDriver().findElement(ENTER_PASSWORD).sendKeys(input);
+        getDriver().findElement(PASSWORD_INPUT_LOCATOR).sendKeys(input);
         return this;
     }
 
-    public LoginPage login() {
-        getDriver().findElement(LOGIN);
+    public LoginPage clickLoginButton() {
+        getDriver().findElement(LOGIN_BUTTON_LOCATOR).click();
         return this;
     }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,16 +3,16 @@ import org.junit.jupiter.api.Test;
 
 
 public class LoginTest extends BaseTest {
-    private final LoginPage puppySearchPage = new LoginPage();
+    private final LoginPage loginPage = new LoginPage();
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "password";
 
     @Test
     public void loginTest() {
-        puppySearchPage
+        loginPage
                 .openPuppyPage()
                 .enterUsername(USERNAME)
                 .enterPassword(PASSWORD)
-                .login();
+                .clickLoginButton();
     }
 }
