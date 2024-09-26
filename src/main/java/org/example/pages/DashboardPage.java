eb9e3b10dcb376f0bddf8d1d479c7d6b706a7a0c
Kordel <Strelok.deth@yandex.ru>
2024-09-26 20:25:14 +0300

    Added DashboardCheck

---
+++ src/main/java/org/example/pages/DashboardPage.java
@@ -0,0 +1,16 @@
+package org.example.pages;
+
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+
+import static org.example.core.DriverHolder.getDriver;
+
+public class DashboardPage {
+
+    private static final By DASHBOARD_TITLE_LOCATOR = By.xpath("//h1[text()='Dashboard']");
+
+    public DashboardPage checkDashboardIsDisplayed() {
+        Assertions.assertTrue(getDriver().findElement(DASHBOARD_TITLE_LOCATOR).isDisplayed());
+        return this;
+    }
+}
