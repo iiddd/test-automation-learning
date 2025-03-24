f22d5d7c15cbc764d668c7f8cc8a5adaf8da81ff
Andrey Merkulov <merkulov.andrej@gmail.com>
2025-03-24 22:22:53 +0100

    Added conditional wait for dashboard page

--- src/main/java/org/example/pages/DashboardPage.java
+++ src/main/java/org/example/pages/DashboardPage.java
@@ -2,6 +2,10 @@ package org.example.pages;
 
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.example.core.DriverHolder.getDriver;
 
@@ -10,6 +14,8 @@ public class DashboardPage extends BasePage {
     private static final By DASHBOARD_TITLE_LOCATOR = By.xpath("//h1[text()='Dashboard']");
 
     public DashboardPage checkDashboardIsDisplayed() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(getDriver().findElement(DASHBOARD_TITLE_LOCATOR)));
         Assertions.assertTrue(getDriver().findElement(DASHBOARD_TITLE_LOCATOR).isDisplayed());
         return this;
     }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,8 +1,8 @@
 Feature: Login
-#  TODO: Doesn't work on CI
-#  Scenario: Login. Happy flow
-#    Given The User navigates to the Login page
-#    When The User inputs "admin" as a username
-#    And The User inputs "password" as a password
-#    And The User clicks Login button
-#    Then The User is on Dashboard page
+
+  Scenario: Login. Happy flow
+    Given The User navigates to the Login page
+    When The User inputs "admin" as a username
+    And The User inputs "password" as a password
+    And The User clicks Login button
+    Then The User is on Dashboard page
